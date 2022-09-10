e26ccd0be078f58d192df8a7e296ca21b2479214
ggyul <wakegyul@gmail.com>
2022-09-10 13:24:42 +0900

    design: 탭에 뜨는 title 변경한다

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomePage({ data }) {
 
   return (
     <Layout>
-      <Seo title="Home" />
+      <Seo title="개발 농장" />
       <Bio author={author} language={language} />
       <PostTabs
         posts={posts}
